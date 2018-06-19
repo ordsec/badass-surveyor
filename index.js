eb644e7f07a368776b0a609cc5d46d7bdee97346
David Ordovskiy <david.ordovsky@gmail.com>
2018-06-19 09:12:14 -0400

    set up mongoose and connect to mlab

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express'),
       app = express();
 
+const mongoose = require('mongoose');
+
 // execute that file, since there's nothing exported from it
 require('./services/passport');
 
 // import authRoutes.js and call it immediately with app obj
 require('./routes/authRoutes')(app);
+
+const keys = require('./config/keys');
+
+// connect to mlab instance
+mongoose.connect(keys.mongoDevDbUri);
 
 const PORT = process.env.PORT || 5000;
 
