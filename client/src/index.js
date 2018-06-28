98e690ad3bdecb3cb6c75419e3f981befe99fd61
David Ordovskiy <david.ordovsky@gmail.com>
2018-06-28 11:36:32 -0400

    wire up redux with a dummy reducer and empty initial state

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
+const store = createStore(
+  () => [],
+  {},
+  applyMiddleware()
+);
+
+const jsx = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  jsx, document.getElementById('root')
 );
