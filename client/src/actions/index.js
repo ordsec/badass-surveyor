2d10471cea904736653e97c3cf557b0004946e99
David Ordovskiy <david.ordovsky@gmail.com>
2018-06-29 14:29:45 -0400

    refactor fetchUser action generator with async/await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios.get('/api/current-user')
-      .then(res => dispatch({
-        type: FETCH_USER,
-        payload: res
-      }));
-  }
+// with redux-thunk, this action generator returns
+// a function rather than an action object. the returned
+// function accepts a `dispatch`, which is called once
+// the async part is done, and there's some data from
+// the server - only then the FETCH_USER action is
+// dispatched.
+export const fetchUser = () => async (dispatch) => {
+  const res = await axios.get('/api/current-user');
+
+  dispatch({ type: FETCH_USER, payload: res });
 };
