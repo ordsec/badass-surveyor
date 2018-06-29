edb185c0c8f233a9cd0052ec0beb9da54ed443a1
David Ordovskiy <david.ordovsky@gmail.com>
2018-06-29 10:29:40 -0400

    import materialize and add preliminary styling for the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">
+            Badass Surveyor
+          </a>
+          <ul className="right">
+            <li>
+              <a href="#">Sign in with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
