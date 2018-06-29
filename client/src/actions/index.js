1d7e7d1d328dfb70ca28a13f1c2db6a816331005
David Ordovskiy <david.ordovsky@gmail.com>
2018-06-29 14:19:14 -0400

    finish & hook up fetchUser action to check if user logged in

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
-  axios.get('/api/current_user');
+export const fetchUser = () => {
+  return function(dispatch) {
+    axios.get('/api/current-user')
+      .then(res => dispatch({
+        type: FETCH_USER,
+        payload: res
+      }));
+  }
 };
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 
@@ -7,17 +9,25 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => (
-  <div className="container">
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Route path="/" exact={true} component={Landing} />
-        <Route path="/surveys" exact={true} component={Dashboard} />
-        <Route path="/surveys/new" component={SurveyNew} />
-      </div>
-    </BrowserRouter>
-  </div>
-);
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" exact={true} component={Landing} />
+            <Route path="/surveys" exact={true} component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
