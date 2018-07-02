81ced1fa0eb96fcafc76cf46641e5f6736f03d91
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-02 13:14:54 -0400

    style the add credits button and the payment form

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,6 +5,8 @@ export default class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="Badass Surveyor"
+        description="$5.00 for 5 email credits"
         // amount is in USD cents
         amount={500}
         // this is not a token but a callback
@@ -14,7 +16,11 @@ export default class Payments extends Component {
         // onTokenReceived or something
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
     );
   }
 }
