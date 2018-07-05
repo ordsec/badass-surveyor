81a9cf032b35b2bb5951e159a73bc002496a0317
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-05 08:39:04 -0400

    make settings to serve up the client side properly in prod

--- index.js
+++ index.js
@@ -40,6 +40,20 @@ require('./routes/billingRoutes')(app);
 // connect to mlab instance
 mongoose.connect(keys.mongoDbUri);
 
+if (process.env.NODE_ENV === 'production') {
+  // serve up production assets (main.js or main.css)
+  app.use(express.static('client/build'));
+
+  // serve up index.html if route is not recognized
+  // (it might be a react-router route)
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, 'client', 'build', 'index.html')
+    );
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
