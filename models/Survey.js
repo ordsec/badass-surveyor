6b0f32b47dab94c73f90c852ad2cd3e5ea59459b
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-06 16:59:32 -0400

    associate surveys with users, add timestamps

--- models/Survey.js
+++ models/Survey.js
@@ -7,8 +7,9 @@ const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  // array containing RecipientSchema instances
+  // array containing RecipientSchema subdocument instances
   recipients: [RecipientSchema],
+  // count yes/no responses
   yes: {
     type: Number,
     default: 0
@@ -16,7 +17,20 @@ const surveySchema = new Schema({
   no: {
     type: Number,
     default: 0
-  }
+  },
+  // every Survey belongs to a particular User.
+  // the underscore is not required, but is used by convention
+  // to indicate a reference field
+  _user: {
+    // this will be the ID of the user who owns the record
+    type: Schema.Types.ObjectId,
+    // reference to the 'users' collection
+    ref: 'User'
+  },
+  // when the survey was sent
+  dateSent: Date,
+  // when the last response was received
+  lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
