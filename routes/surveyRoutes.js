860c2cdd061e5b9fbcbd7b8bf255fa0ac4e6ffa9
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-07 11:08:35 -0400

    start putting together the logic in POST /api/surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,20 @@
+const mongoose = require('mongoose');
+
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+// use this instead of requiring the model in order to avoid
+// testing issues
+const Survey = mongoose.model('surveys');
+
 module.exports = (app) => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
+    const newSurvey = new Survey({
+      title,
+      subject,
+      body
+    });
   });
 };
