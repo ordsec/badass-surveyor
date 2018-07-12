aba0ef0249124c81746085ae3b3288b9cafa94c2
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-12 17:39:26 -0400

    install redux-form and add SurveyNew container for the upcoming form

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        Form goes heah
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
