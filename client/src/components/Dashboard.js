e3b52a6dea9b2e33d8746ae8758e0e2f8ef8e380
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-12 14:22:29 -0400

    set up the Dashboard component with 'add' button

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = () => (
+  <div>
+    This the dashboard, yo
+    <div className="fixed-action-btn">
+      <a className="btn-floating btn-large red">
+        <i className="material-icons">chat_bubble_outline</i>
+      </a>
+    </div>
+  </div>
+);
+
+export default Dashboard;
