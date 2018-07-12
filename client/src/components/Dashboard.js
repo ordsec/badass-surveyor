a6a4f41121ffa9ef7327053304e02890300122f0
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-12 14:25:42 -0400

    convert <a> to <Link> in Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => (
   <div>
     This the dashboard, yo
     <div className="fixed-action-btn">
-      <a className="btn-floating btn-large red">
+      <Link
+        to="/surveys/new"
+        className="btn-floating btn-large red"
+      >
         <i className="material-icons">chat_bubble_outline</i>
-      </a>
+      </Link>
     </div>
   </div>
 );
