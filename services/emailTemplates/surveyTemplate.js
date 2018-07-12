7541830474d51ff17314140258c5583847853226
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-11 21:04:51 -0400

    update the email template with some HTML

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,18 @@
 module.exports = (survey) => {
-  return `<div>${survey.body}</div>`;
+  return `
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <h3>We want to hear from you!</h3>
+          <p>${survey.body}</p>
+          <div>
+            <a href="http://localhost:3000">Oh yeah!</a>
+          </div>
+          <div>
+            <a href="http://localhost:3000">Nope, not a fan.</a>
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
 };
