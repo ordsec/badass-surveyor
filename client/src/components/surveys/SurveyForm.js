9473205f12f43e87c116b019891ba660ce1312c9
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-14 11:35:17 -0400

    add preliminary form setup

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends Component {
   render() {
     return (
       <div>
-        And the ACTUAL form goes heah!
+        <form onSubmit={
+          // handleSubmit is provided by redux-form -
+          // it takes our functions that describes
+          // what to do with the values in the form
+          this.props.handleSubmit((values) => console.log(values))
+        }>
+          <Field
+            type="text"
+            name="surveyTitle"
+            component="input"
+          />
+          <button type="submit">Let's do it</button>
+        </form>
       </div>
     );
   }
