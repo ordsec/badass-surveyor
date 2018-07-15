4e21fbb0d4e49db0be982c4ba229c5ef0e830f44
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-15 08:16:35 -0400

    add Cancel button to the form, style buttons

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,14 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 import SurveyField from './SurveyField';
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title' },
   { label: 'Subject Line', name: 'subject' },
-  { label: 'Email Boydy', name: 'body' },
+  { label: 'Email Body', name: 'body' },
   { label: 'Recipient List', name: 'recipients' }
 ];
 
@@ -34,7 +35,14 @@ class SurveyForm extends Component {
           this.props.handleSubmit((values) => console.log(values))
         }>
           {this.renderFields()}
-          <button type="submit">Let's do it</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+            <i className="material-icons right">clear</i>
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            NEXT
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
