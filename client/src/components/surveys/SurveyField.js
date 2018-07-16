49a165cd47ebf59ad595d994c54268bf4c0a36d4
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-16 10:50:55 -0400

    style validation errors

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -3,8 +3,12 @@ import React from 'react';
 const SurveyField = ({ input, label, meta: { error, touched } }) => (
   <div>
     <label>{label}</label>
-    <input {...input} />
-    {touched && error}
+    <input {...input} style={{ marginBottom: '5px' }} />
+    <div className="red-text" style={{
+      marginBottom: '20px'
+    }}>
+      {touched && error}
+    </div>
   </div>
 );
 
