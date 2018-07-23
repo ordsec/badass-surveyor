a1e099a7238cba03e9081618ea93dec99a9edd66
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-23 10:52:59 -0400

    render the list of surveys

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { fetchSurveys } from '../../actions';
 
@@ -8,10 +9,39 @@ class SurveyList extends Component {
     this.props.fetchSurveys();
   }
 
+  renderSurveys() {
+    if (!this.props.surveys.length) {
+      return (
+        <h5 style={{ 'text-align': 'center' }}>
+          No surveys yet!&nbsp;
+          <Link to="/surveys/new">Create one</Link>, perhaps?
+        </h5>
+      );
+    }
+
+    return this.props.surveys.map((survey, idx) => {
+      return (<div className="card deep-orange lighten-5" key={idx}>
+        <div className="card-content">
+          <span className="card-title">{survey.title}</span>
+          <p>
+            {survey.body}
+          </p>
+          <p className="right">
+            Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+          </p>
+        </div>
+        <div className="card-action">
+          <a>Yes: {survey.yes}</a>
+          <a>No: {survey.no}</a>
+        </div>
+      </div>);
+    });
+  }
+
   render() {
     return (
       <div>
-        Surveys coming soon...
+        {this.renderSurveys()}
       </div>
     );
   }
